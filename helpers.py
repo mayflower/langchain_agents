073d88889dd5b19143a01a9ee1c6a2672e881c37
LorAutumn <lorenz.herbst@gmx.net>
2024-03-11 12:23:29 +0000

    update agent output streamer to latest astream_events

--- helpers.py
+++ helpers.py
@@ -35,7 +35,17 @@ def embeddings():
 def formatted_output_writer(data):
     return data.get("agent_outcome").return_values.get("output")
 
-async def graph_agent_llm_output_streamer(app, inputs):
+# Only streams the final LLM output from the agent. Uses the latest method which is astream_events (in BETA).
+async def graph_agent_llm_output_streamer_events(app, inputs):
+    async for event in app.astream_events(inputs, version="v1"):
+        ev = event["event"]
+        if ev == "on_chat_model_stream":
+            function_call_chunk = event["data"]["chunk"].additional_kwargs.get("function_calls", None)
+            if function_call_chunk is None:
+                print(event["data"]["chunk"].content, end="", flush=True)
+
+# Only streams the final LLM output from the agent. Uses astream_log which is stable.
+async def graph_agent_llm_output_streamer_log(app, inputs):
     async for output in app.astream_log(inputs, include_types=["llm"]):
         for op in output.ops:
             if op["path"] == "/streamed_output/-":
@@ -46,5 +56,7 @@ async def graph_agent_llm_output_streamer(app, inputs):
             ):
                 # because we chose to only include LLMs, these are LLM tokens
                 token_content = op["value"].content
-                if token_content:
+                function_call = op["value"].additional_kwargs.get("function_calls", None)
+                if token_content and not function_call:
                     print(op["value"].content, end="", flush=True)
+
