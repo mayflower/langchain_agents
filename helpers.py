147f2d5c3deddd1efda1461fb9c8975e0abba5ed
LorAutumn <lorenz.herbst@gmx.net>
2024-03-12 16:33:14 +0100

    add openai_model argument to llm()

--- helpers.py
+++ helpers.py
@@ -6,11 +6,11 @@ from langchain_openai import OpenAIEmbeddings, AzureOpenAIEmbeddings
 load_dotenv()
 
 
-def llm(temperature: float = 0.7):
+def llm(temperature: float = 0.7, openai_model: str = None):
     if os.environ["OPENAI_API_KEY"]:
-        return ChatOpenAI(
-            model=os.environ["OPENAI_MODEL"], temperature=temperature, streaming=True
-        )
+        model = openai_model if openai_model else os.environ.get(
+            "OPENAI_MODEL")
+        return ChatOpenAI(model=model, temperature=temperature, streaming=True)
     elif os.environ["AZURE_OPENAI_API_KEY"]:
         return AzureChatOpenAI(
             azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
@@ -36,15 +36,20 @@ def formatted_output_writer(data):
     return data.get("agent_outcome").return_values.get("output")
 
 # Only streams the final LLM output from the agent. Uses the latest method which is astream_events (in BETA).
+
+
 async def graph_agent_llm_output_streamer_events(app, inputs):
     async for event in app.astream_events(inputs, version="v1"):
         ev = event["event"]
         if ev == "on_chat_model_stream":
-            function_call_chunk = event["data"]["chunk"].additional_kwargs.get("function_calls", None)
+            function_call_chunk = event["data"]["chunk"].additional_kwargs.get(
+                "function_calls", None)
             if function_call_chunk is None:
                 print(event["data"]["chunk"].content, end="", flush=True)
 
 # Only streams the final LLM output from the agent. Uses astream_log which is stable.
+
+
 async def graph_agent_llm_output_streamer_log(app, inputs):
     async for output in app.astream_log(inputs, include_types=["llm"]):
         for op in output.ops:
@@ -56,7 +61,7 @@ async def graph_agent_llm_output_streamer_log(app, inputs):
             ):
                 # because we chose to only include LLMs, these are LLM tokens
                 token_content = op["value"].content
-                function_call = op["value"].additional_kwargs.get("function_calls", None)
+                function_call = op["value"].additional_kwargs.get(
+                    "function_calls", None)
                 if token_content and not function_call:
                     print(op["value"].content, end="", flush=True)
-
