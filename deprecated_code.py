afad399371a76040863b78cd37a5801cc500834c
Konstantin Sabass <konstantin.sabass@mayflower.de>
2024-03-21 14:58:44 +0100

    rework cpal

--- deprecated_code.py
+++ deprecated_code.py
@@ -1,7 +1,8 @@
 import os
+
 from dotenv import load_dotenv
-from langchain_openai import AzureChatOpenAI, ChatOpenAI
 from langchain_core.runnables import ConfigurableField
+from langchain_openai import AzureChatOpenAI, ChatOpenAI
 
 load_dotenv()
 
@@ -20,8 +21,8 @@ llm = openai_llm.configurable_alternatives(
 # Man sollte sich aber merken, was er tut, um bei Bedarf hierher zurück zu springen um Code zu kopieren:
 # Definiere ein LLM, das zur Laufzeit konfiguriert werden kann (OpenAI, AzureOpenAI, Temperatur....)
 # Diese Langchain-Funktionalität wird sich beim Programmieren später einmal sehr nützlich erweisen.
-from langchain_openai import AzureChatOpenAI, ChatOpenAI
 from langchain_core.runnables import ConfigurableField
+from langchain_openai import AzureChatOpenAI, ChatOpenAI
 
 openai_llm = ChatOpenAI(model=os.environ["OPENAI_MODEL"]).configurable_fields(
     temperature=ConfigurableField(id="temperature", is_shared=True)
@@ -40,15 +41,13 @@ llm = openai_llm.configurable_alternatives(
 # Die Schnittstelle kümmert sich außerdem um Integrität!
 # Hier kann ein Nutzer eine Funktion aufrufen, die vektorisiert und das Embedding-Modell selbst wählen.
 
-from langchain_openai import OpenAIEmbeddings, AzureOpenAIEmbeddings
 from langchain_core.runnables import (
     ConfigurableFieldSingleOption,
     RunnableBinding,
     RunnableConfig,
     chain,
 )
-
-load_dotenv()
+from langchain_openai import AzureOpenAIEmbeddings, OpenAIEmbeddings
 
 
 class EmbedText(RunnableBinding):
@@ -82,3 +81,32 @@ embed_text = EmbedText().configurable_fields(
 )
 
 embed_text.config_schema().schema().get("definitions")
+
+question = (
+    "Five houses stand in a row."
+    "Each house has a different color."
+    "The nationalities of the house residents are different."
+    "Each resident prefers a certain drink."
+    "Each resident smokes a certain brand of cigarettes."
+    "Each resident keeps a certain pet."
+    "The colors are blue, yellow, green, red, and white."
+    "The nationalities are Danish, German, British, Norwegian, and Swedish."
+    "The drinks are Beer, Coffee, Milk, Tea, and Water."
+    "The cigarette brands are Dunhill, Marlboro, Pall Mall, Rothmans, and Winfield."
+    "The pets are Fish, Dog, Cat, Horse, and Bird."
+    "The British lives in the red house."
+    "The Swedish keeps a dog."
+    "The Danish likes to drink tea."
+    "The green house is to the left of the white house."
+    "The owner of the green house drinks coffee."
+    "The person who smokes Pall Mall keeps a bird."
+    "The man who lives in the middle house drinks milk."
+    "The owner of the yellow house smokes Dunhill."
+    "The Norwegian lives in the first house."
+    "The Marlboro smoker lives next to the one who keeps a cat."
+    "The man who keeps a horse lives next to the one who smokes Dunhill."
+    "The Winfield smoker likes to drink beer."
+    "The Norwegian lives next to the blue house."
+    "The German smokes Rothmans."
+    "If the Marlboro smoker has a neighbor who drinks water, which resident owns the fish?"
+)
