3994f8e9403388e3868ad0d777f13c4382e1b137
LorAutumn <lorenz.herbst@gmx.net>
2024-05-28 15:42:06 +0200

    add speech assistant

---
+++ speech/generator.py
@@ -0,0 +1,107 @@
+from langchain_google_vertexai import ChatVertexAI
+import queue
+import threading
+import random
+import os
+import re
+import google.cloud.texttospeech as tts
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+class GenerateAndPlayBack:
+    """Generates an LLM answer via Gemini Pro and plays it back via Google TTS."""
+
+    def __init__(self, model=ChatVertexAI(model="gemini-1.5-pro-preview-0409")):
+        self.model = model
+        self.sentence_queue = queue.Queue()
+        self.wav_queue = queue.Queue()
+        self.sentences = []
+
+    """Converts text to speech using Google TTS and saves it to a .wav file."""
+
+    def text_to_wav(self, voice_name: str, text: str):
+        language_code = "de-DE"
+        text_input = tts.SynthesisInput(text=text)
+        voice_params = tts.VoiceSelectionParams(
+            language_code=language_code, name="de-DE-Standard-C"
+        )
+        audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16)
+
+        client = tts.TextToSpeechClient()
+        response = client.synthesize_speech(
+            input=text_input,
+            voice=voice_params,
+            audio_config=audio_config,
+        )
+
+        filename = f"{voice_name}.wav"
+        with open(filename, "wb") as out:
+            out.write(response.audio_content)
+        return filename
+
+    """Plays a .wav file using PyDub."""
+
+    def play_wav(self, filename: str):
+        audio = AudioSegment.from_wav(filename)
+        play(audio)
+
+    """Processes the sentence queue and generates .wav files for each sentence."""
+
+    def process_queue(self):
+        while True:
+            item = self.sentence_queue.get()
+            if item is None:
+                self.wav_queue.put("STOP_QUEUE_WAV")
+                break
+
+            filename = self.text_to_wav(str(random.randint(1, 1000)), item)
+            self.wav_queue.put(filename)
+            print("\n", "sentences", item, "\n")
+
+    """Processes the .wav queue and plays back the .wav files."""
+
+    def process_wav_queue(self):
+        while True:
+            item = self.wav_queue.get()
+            print("item", item)
+            if item is None:
+                continue
+            if item == "STOP_QUEUE_WAV":
+                break
+            print("playing", item)
+            self.play_wav(item)
+            os.remove(item)
+
+    """Generates an LLM answer and puts each sentence in the sentence queue for processing
+    
+    Is configured for text generation with Gemini Pro. For use with other models the generate_and_play may need to be adjusted.
+    This is because Gemini Pro streaming does not return single tokens but chunks of text. Sentences are actually split by . after a character.
+    It will not work if single tokens are returned by different models."""
+    def generate_and_play(self, result):
+        """Start the processing queue threads."""
+        t = threading.Thread(target=self.process_queue)
+        tw = threading.Thread(target=self.process_wav_queue)
+        t.start()
+        tw.start()
+
+        actual_sentence = ""
+        for chunk in self.model.stream(result):
+            """Collect tokens and split them into sentences."""
+            match = re.search(r"[a-zA-Z]\.", chunk.content)
+            if match:
+                start, end = match.span()
+                start_string = chunk.content[: start + 2]
+                end_string = chunk.content[end:]
+                actual_sentence += start_string
+                self.sentence_queue.put(actual_sentence)
+                actual_sentence = end_string
+            else:
+                actual_sentence += chunk.content
+        """Put the last sentence in the queue and stop the queue by putting a None."""
+        self.sentence_queue.put(actual_sentence)
+        self.sentence_queue.put(None)
+
+        """Wait for the threads to finish."""
+        t.join()
+        tw.join()
---
+++ speech/google_speech_assist.py
@@ -0,0 +1,223 @@
+import asyncio
+import queue
+import sys
+import os
+from dotenv import load_dotenv
+
+from google.cloud import speech
+
+from langchain_google_vertexai import ChatVertexAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+import pyaudio
+
+# from helpers import llm
+
+import google.cloud.texttospeech as tts
+from pydub import AudioSegment
+from pydub.playback import play
+from generator import GenerateAndPlayBack
+
+# from helpers import graph_agent_llm_output_streamer_events
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+
+load_dotenv()  # take environment variables from .env.
+api_key = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatVertexAI(model="gemini-1.5-pro-preview-0409", google_api_key=api_key)
+# llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-preview-0409", google_api_key=api_key)
+
+"""Records audio and answers in a streaming manner."""
+
+
+class MicrophoneStream:
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self: object, rate: int = RATE, chunk: int = CHUNK) -> None:
+        """The audio -- and generator -- is guaranteed to be on the main thread."""
+        self._rate = rate
+        self._chunk = chunk
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self: object) -> object:
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(
+        self: object,
+        type: object,
+        value: object,
+        traceback: object,
+    ) -> None:
+        """Closes the stream, regardless of whether the connection was lost or not."""
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(
+        self: object,
+        in_data: object,
+        frame_count: int,
+        time_info: object,
+        status_flags: object,
+    ) -> object:
+        """Continuously collect data from the audio stream, into the buffer.
+
+        Args:
+            in_data: The audio data as a bytes object
+            frame_count: The number of frames captured
+            time_info: The time information
+            status_flags: The status flags
+
+        Returns:
+            The audio data as a bytes object
+        """
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self: object) -> object:
+        """Generates audio chunks from the stream of audio data in chunks.
+
+        Args:
+            self: The MicrophoneStream object
+
+        Returns:
+            A generator that outputs audio chunks.
+        """
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+
+
+def listen_print_loop(responses: object) -> str:
+    """Iterates through server responses and prints them.
+
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+
+    Args:
+        responses: List of server responses
+
+    Returns:
+        The transcribed text.
+    """
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+        transcript = result.alternatives[0].transcript
+        overwrite_chars = " " * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + "\r")
+            sys.stdout.flush()
+
+            num_chars_printed = len(transcript)
+
+        else:
+            print("final", transcript + overwrite_chars)
+            break
+
+            # # Exit recognition if any of the transcribed phrases could be
+            # # one of our keywords.
+            # if re.search(r"\b(exit|quit)\b", transcript, re.I):
+            #     print("Exiting..")
+            #     break
+
+    return transcript
+
+
+async def main() -> None:
+    """Transcribe speech from audio file, generate answer and play back."""
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+    language_code = "de-DE"  # a BCP-47 language tag
+
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=language_code,
+    )
+
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (
+            speech.StreamingRecognizeRequest(audio_content=content)
+            for content in audio_generator
+        )
+
+        responses = client.streaming_recognize(streaming_config, requests)
+
+        # Now, put the transcription responses to use.
+        result = listen_print_loop(responses)
+
+        print("result:", result)
+
+        process_result = GenerateAndPlayBack()
+        process_result.generate_and_play(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
